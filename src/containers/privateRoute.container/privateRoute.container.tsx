cd5cdd4273c91c7ceff2c806218af61437e0800c
sammytran90 <32162425+sammytran90@users.noreply.github.com>
2019-05-26 08:06:41 +0700

    fix

--- src/containers/privateRoute.container/privateRoute.container.tsx
+++ src/containers/privateRoute.container/privateRoute.container.tsx
@@ -2,16 +2,28 @@ import React, { ComponentType, Component } from 'react';
 import { Route, Redirect, RouteType } from 'react-router-dom';
 import { authGuard } from '../../auth/auth.guard';
 
-export const PrivateRoute = <P extends object>({ Component, ...rest }: { Component: ComponentType<any> }) => (
+export const PrivateRoute = ({ component, ...rest }: { component: ComponentType<any> }) => (
     <Route {...rest} render={(props: any) => {
         authGuard.isAuthenticated ? <Component {...props} /> : <Redirect to='/' />
     }} />
 )
 
-// class PrivateRoute extends Component<any, any> {
+// interface PrivateRouteProps {
+//     Component: ComponentType,
+//     path: string
+// }
+
+// class PrivateRoute extends Component<PrivateRouteProps> {
+//     constructor(props: PrivateRouteProps) {
+//         super(props)
+//     }
 //     render() {
 //         return (
-//             <Route {...rest} />
+//             <Route path={this.props.path} render={(props: any) => {
+//                 authGuard.isAuthenticated ? <Component {...props}></Component> : <Redirect to='/' />
+//             }} />
 //         )
 //     }
 // }
+
+// export default PrivateRoute;
